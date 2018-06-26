0533df4464c8227018c7a87da2d5748c1339e955
Natalie Ghidali <nghidali@fb.com>
2018-06-26 13:15:37 -0700

    Initial Commit

---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Natalie Ghidali on 6/26/18.
+//  Copyright © 2018 Natalie Ghidali. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
